c17397b03c5e0b000b9664e24dd461b8b2e3b1ce
Hesto <losiakp@gmail.com>
2016-08-29 15:40:41 +0200

    fix 22

--- src/Traits/CanReplaceKeywords.php
+++ src/Traits/CanReplaceKeywords.php
@@ -15,28 +15,40 @@ trait CanReplaceKeywords
     {
         $name = $this->getNameInput();
 
+        $name = str_plural($name);
+
         $plural = [
             '{{pluralCamel}}',
             '{{pluralSlug}}',
             '{{pluralSnake}}',
+            '{{pluralClass}}',
         ];
 
         $singular = [
             '{{singularCamel}}',
             '{{singularSlug}}',
             '{{singularSnake}}',
+            '{{singularClass}}',
         ];
 
-        $replace = [
+        $replacePlural = [
             camel_case($name),
             str_slug($name),
             snake_case($name),
+            ucfirst(camel_case($name)),
+        ];
+
+        $replaceSingular = [
+            str_singular(camel_case($name)),
+            str_singular(str_slug($name)),
+            str_singular(snake_case($name)),
+            str_singular(ucfirst(camel_case($name))),
         ];
 
 
 
-        $template = str_replace($plural, $replace, $template);
-        $template = str_replace($singular, $replace, $template);
+        $template = str_replace($plural, $replacePlural, $template);
+        $template = str_replace($singular, $replaceSingular, $template);
         $template = str_replace('{{Class}}', ucfirst(camel_case($name)), $template);
 
         return $template;
